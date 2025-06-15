f351311d37c974f114f2bad2ca35274ec5ca1ebf
suraj <surajakula.19@gmail.com>
2025-06-14 20:41:18 -0400

    refactored

--- src/main/java/com/example/cricket/repositories/PlayerRepository.java
+++ src/main/java/com/example/cricket/repositories/PlayerRepository.java
@@ -3,7 +3,8 @@ package com.example.cricket.repositories;
 import com.example.cricket.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<Player,Long> {
-
-
+    Optional<Player> findByNameAndRole(String name, String role);
 }
